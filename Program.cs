c4e38ec845ce721fff7d1537aca44c4bbdae29dc
AndreyKerges <AndreyKergesProfissional@gmail.com>
2023-03-09 17:46:27 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nome das cidades do litoral sul de São Paulo:");
+        Console.WriteLine();
+        string[] cidades = {"Peruíbe", "Itanhaém", "Mongaguá", "Praia Grande", "São Vicente", "Santos", "Guarujá", "Bertioga", "São Sebastião", "Ilhabela", "Caraguatatuba", "Ubatuba"};
+        
+        foreach(string cidade in cidades)
+        {
+            Console.WriteLine(cidade);
+        }
+    }
+}
